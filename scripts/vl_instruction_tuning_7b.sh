0eb080f23e1fe8272f31bc6ab10146b867dbb31c
ScarlettSun9 <53145308+ScarlettSun9@users.noreply.github.com>
2024-03-16 19:44:18 +0800

    Add files via upload

---
+++ scripts/vl_instruction_tuning_7b.sh
@@ -0,0 +1,21 @@
+torchrun --nproc_per_node 1 train.py \
+    --llm_model 7B\
+    --llama_model_path ../meta-llama-2/llama-2-7b/ \
+    --tokenizer_path ../Tokenizer/merged_tokenizer_hf/ \
+    --data_path ../PT_Dataset/ \
+    --max_seq_len 512 \
+    --batch_size 1 \
+    --accum_iter 1 \
+    --epochs 1 \
+    --warmup_epochs 0.1 \
+    --blr 9e-3 \
+    --weight_decay 0.02 \
+    --output_dir ./LaVIN-7B-VLIT/\
+    --adapter_type attn\
+    --adapter_dim 8\
+    --adapter_scale 1\
+    --n_prompt 6 \
+    --prompt_format QCM-ALE \
+    --temperature 5.\
+    --visual_adapter_type router \
+    --do_pretrain
